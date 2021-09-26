0f043e936fb429684f13ff79482defe50603d0c9
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-09-26 10:10:00 +0200

    menu dodane

--- witryna-spa/src/app/app.module.ts
+++ witryna-spa/src/app/app.module.ts
@@ -6,11 +6,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { OnasComponent } from './onas/onas.component';
-
+//Komponenty Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { NawigacjaComponent } from './nawigacja/nawigacja.component';
 @NgModule({
   declarations: [
     AppComponent,
-    OnasComponent
+    OnasComponent,
+    NawigacjaComponent
   ],
   imports: [
     BrowserModule,
@@ -20,7 +25,10 @@ import { OnasComponent } from './onas/onas.component';
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
